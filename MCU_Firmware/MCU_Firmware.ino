54b033e7fc80a778f4fac0b5c8e46a6dc7492823
MolecularPrism <54078316+MolecularPrism@users.noreply.github.com>
2024-03-07 21:58:31 -0800

    MCU_Firmware.ino is the working version

--- MCU_Firmware/MCU_Firmware.ino
+++ MCU_Firmware/MCU_Firmware.ino
@@ -1,30 +1,48 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
 // PID controller gains
 #define Kp 1.0
 #define Ki 0.1
 #define Kd 0.01
 
+// WSF Constants
+#define WSF_SAMPLE_COUNT 7 
+const double WSF_CONST_LOOKUP_TABLE[7] = {100, 51.3417, 26.3597, 13.53353, 6.948345, 3.567399, 1.831564};
+#define WST_CONST_SUM 203.582238
+
 // Define maximum and minimum output values
 #define MAX_OUTPUT 100.0
-#define MIN_OUTPUT 0.0
+#define MIN_OUTPUT 100.0
 
 
 //pin def
 const int trigPin = 13;
 const int echoPin = 12;
-const int pwmPin1 = 11;
+const int pwmPin1 = 8;
+
+//WSF variables
+double raw_deriv_samples[WSF_SAMPLE_COUNT] = {};
+double filtered_derivative;
+int count = 0;
 
 
 //variable def
 long duration;
 int distance;
-double desired_position = 90.0; // Desired position of the robotic arm
+double desired_position = 100.0; // Desired position of the robotic arm
 double current_position;        // Current position of the robotic arm
 double integral = 0.0, derivative, previous_error = 0.0;
 double control_signal_output;
 int saturation = 0;
+int error;
+unsigned long prev_time = 0;
+
 
 //Timer1 ISR variables
-int period_ms1 = 100;
+double period_us1;
 volatile bool pwmState1 = false;
 volatile unsigned long previousMillis1 = 0;
 volatile unsigned long onTime_ms1;
@@ -32,7 +50,7 @@ volatile unsigned long offTime_ms1;
 volatile float dutyCycle_fraction1;  // Duty cycle in percentage
 volatile unsigned long time_buffer_ms1 = offTime_ms1;
 
-double PID_Controller(int error, int* saturation);
+void PID_Controller(int error, int* saturation);
 
 
 
@@ -43,18 +61,19 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(pwmPin1, OUTPUT);
 
-  // Calculate on-time and off-time based on duty cycle
-  onTime_ms1 = period_ms1 * dutyCycle_fraction1;
-  offTime_ms1 = period_ms1 - onTime_ms1;
+  period_us1 = 4000 + 120;
+
 
   // Configure Timer1 for PWM generation
   noInterrupts();           // Disable interrupts during setup
   TCCR1A = 0;               // Clear Timer1 control registers
   TCCR1B = 0;
   TCNT1  = 0;               // Timer1 counter starts from 0
-  OCR1A = (16000 * offTime_ms1) / 1000 - 1;  // Set PWM period
+  OCR1A = (period_us1 * 16) / 256 - 1;  // Set PWM period
   TCCR1B |= (1 << WGM12);   // Configure Timer1 for CTC mode
-  TCCR1B |= (1 << CS12);    // Set Timer1 prescaler to 8 (timer counts 1/8th of clock speed)
+  TCCR1B |= (1 << CS12);    // Set Timer1 prescaler to 256
+  TCCR1B |= (0 << CS11);
+  TCCR1B |= (0 << CS10);
   TIMSK1 |= (1 << OCIE1A);  // Enable Timer1 compare match interrupt
   interrupts();             // Enable interrupts
 
@@ -62,62 +81,58 @@ void setup() {
 
 // Timer ISR (Interrupt Service Routine)
 ISR(TIMER1_COMPA_vect) {
-  
-  // Update OCR1A to switch off after the on time
-  OCR1A = (16000 * offTime_ms1 * pwmState1 + 16000 * onTime_ms1 * (!pwmState1)) / 1000 - 1;
+  digitalWrite(pwmPin1, HIGH);
+  PID_Controller(error, &saturation);
+  if(saturation){
+    integral = 0.0;
+  }
 
+  OCR1A = (period_us1 * 16) / 256 - 1;
 
-  // Toggle PWM state if duty cycle is not 0
-
-  pwmState1 = !pwmState1;
-  digitalWrite(pwmPin1, pwmState1);
+  digitalWrite(pwmPin1, LOW);
 
 }
 
+double FDD_WSF(double raw_deriv_samples[]){
+  double sum = 0.0;
+  double filtered_Derivative;
 
+  for(int i = 0; i < WSF_SAMPLE_COUNT; i++){
+    sum += WSF_CONST_LOOKUP_TABLE[i] * raw_deriv_samples[i];
+  }
+  filtered_Derivative = sum / WST_CONST_SUM;
 
-int ultrasonic_handler(){
-  // Clear the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  
-  // Set the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Read the echoPin, and calculate the duration in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance (in cm) based on the speed of sound (340 m/s)
-  // and the time it took for the ultrasonic pulse to travel back and forth
-  distance = duration * 0.034 / 2;
-
+  return filtered_Derivative;
 
   
-  // Delay before next measurement
-  delay(20);
-
-  return distance;
 }
+
 
 //for testing purpose, we are going to pass in error
-double PID_Controller(int error, int* saturation){
+void PID_Controller(int error, int* saturation){
 
   // Calculate error
   //error = desired_position - current_position;
 
+  unsigned long current_time = millis(); // Current time
+  double dt = (current_time - prev_time) / 1000.0; // Time difference in seconds
 
   // Update integral term
-  integral += error;
+  integral += error * dt;
 
   // Update derivative term
-  derivative = error - previous_error;
+  derivative = (error - previous_error) / dt;
 
-  // Calculate control signal
-  control_signal_output = Kp * error + Ki * integral + Kd * derivative;
+  raw_deriv_samples[count] = derivative;
+  count++;
 
-   // Limit control signal output
+  if(count == WSF_SAMPLE_COUNT){ 
+    filtered_derivative = FDD_WSF(raw_deriv_samples);
+
+    // Calculate control signal
+    control_signal_output = Kp * error + Ki * integral + Kd * filtered_derivative;
+
+    // Limit control signal output (prevent integration winding)
     if (control_signal_output > MAX_OUTPUT) {
         *saturation = 1; // Set saturation flag
         control_signal_output = MAX_OUTPUT;
@@ -128,31 +143,30 @@ double PID_Controller(int error, int* saturation){
         *saturation = 0; // Clear saturation flag
     }
 
+    //reset count
+    count = 0;
+
+  }
+
   // Update previous error for derivative
   previous_error = error;
-  Serial.print("\nanalog output: ");
-  Serial.print(control_signal_output);
-  Serial.print(" at ");
-  Serial.print(error);
-  Serial.print(" cm\n");
+  prev_time = current_time;
 
+}
 
-  return control_signal_output;
+int analog_to_period(double analog){
+  int period_ms = analog * 5;
+
+  return period_ms;
 }
 
 void loop() {
-  int error = ultrasonic_handler();
-  int analog_out = PID_Controller(error, &saturation);
 
-  //correlate analog_out of PID controller with duty cycle
-  dutyCycle_fraction1 = analog_out / 100.1;
-
-  // Recalculate on-time and off-time based on updated duty cycle fraction
-  onTime_ms1 = period_ms1 * dutyCycle_fraction1;
-  offTime_ms1 = period_ms1 - onTime_ms1;
-
-  if(saturation){
-    integral = 0.0;
+  for(int i = 0; i<1000; i++){
+    Serial.print("\n");
+    Serial.print("i is: ");
+    Serial.print(i);
   }
+
 
 }
